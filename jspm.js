51c795a3f06f0e68d5b38583e673d64f0d715986
Ossama <ossedb@gmail.com>
2014-12-16 15:02:30 +0000

    Added exceptions

--- jspm.js
+++ jspm.js
@@ -1,75 +1,99 @@
-function assertType(obj, type) {
-    return obj.constructor.name === type.name
-}
-
-/*function curry(func,args,space) {
-    var n  = func.length - args.length; //arguments still to come
-    var sa = Array.prototype.slice.apply(args); // saved accumulator array
-    function accumulator(moreArgs,sa,n) {
-        var saPrev = sa.slice(0); // to reset
-        var nPrev  = n; // to reset
-        for(var i=0;i<moreArgs.length;i++,n--) {
-            sa[sa.length] = moreArgs[i];
-        }
-        if ((n-moreArgs.length)<=0) {
-            var res = func.apply(space,sa);
-            // reset vars, so curried function can be applied to new params.
-            sa = saPrev;
-            n  = nPrev;
-            return res;
-        } else {
-            return function (){
-                // arguments are params, so closure bussiness is avoided.
-                return accumulator(arguments,sa.slice(0),n);
-            }
-        }
+var $pattern_match = (function (undefined) {
+    // Exceptions
+    function MatchingExprException(message) {
+        this.message = message;
+        this.name = 'MatchingExprException';
     }
-    return accumulator([],sa,n);
-}*/
 
-var equal_arr = function (arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    
-    for (var i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) // Also check objects
-            return false;
+    var assertType = function (obj, type) {
+        return obj.constructor.name === type.name
     }
-}
 
-// Deal with objects, arrays (head, tail, deep comparison)
-var $wc = {}; // Fix this
-
-var rec_case = function () {
-    
-};
-
-var $match_with = function () {
-    var args = arguments,
-        len = args.length;
-    
-    return function (val) {
-        // Check for type
-        
-        for (var i = 0; i < len; i++) {
-            var matching = args[i];
-            
-            if (val === matching[0] || matching[0] === $wc) {
-                if (matching[1].length === 0)
-                    return matching[1].call(this);
-                else if (matching[1].length === 1)
-                    return matching[1].call(this, val);
-                else {}
-            }
-        }
+    var is_array = function (obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]'; 
     };
-};
 
-var sum = $match_with(
-    [[],                  function () { return 0 }],
-    [rec_case('x', 'xs'), function () { return x + sum (xs) }]
-);
+    /*function curry(func,args,space) {
+        var n  = func.length - args.length; //arguments still to come
+        var sa = Array.prototype.slice.apply(args); // saved accumulator array
+        function accumulator(moreArgs,sa,n) {
+            var saPrev = sa.slice(0); // to reset
+            var nPrev  = n; // to reset
+            for(var i=0;i<moreArgs.length;i++,n--) {
+                sa[sa.length] = moreArgs[i];
+            }
+            if ((n-moreArgs.length)<=0) {
+                var res = func.apply(space,sa);
+                // reset vars, so curried function can be applied to new params.
+                sa = saPrev;
+                n  = nPrev;
+                return res;
+            } else {
+                return function (){
+                    // arguments are params, so closure bussiness is avoided.
+                    return accumulator(arguments,sa.slice(0),n);
+                }
+            }
+        }
+        return accumulator([],sa,n);
+    }*/
 
-/*var fact = fun(
-    [0, function ()  1],
-    [wc, function (n) n * fact(n - 1)]
-);*/
+    var equal_arr = function (arr1, arr2) {
+        if (arr1.length !== arr2.length) return false;
+
+        for (var i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) // Also check objects
+                return false;
+        }
+    }
+
+    // Deal with objects, arrays (head, tail, deep comparison)
+    var $wc = {}; // Fix this
+
+    var rec_case = function () {
+
+    };
+
+    var on = function () {
+        var args = arguments,
+            len = args.length;
+
+        return function (val) {
+            // Type checking
+            var type = val.constructor.name;
+            
+            for (var i = 0; i < len; i++) {
+                var matching = args[i];
+
+                if (!is_array(matching) || matching.length !== 2 || !assertType(matching[0],type))
+                    throw new MatchingExprException('Matching expression not well defined');
+            }
+
+            for (var i = 0; i < len; i++) {
+                var matching = args[i];
+                
+                // Numbers and strings
+                if (val === matching[0] || matching[0] === $wc) {
+                    if (matching[1].length === 0)
+                        return matching[1].call(this);
+                    else if (matching[1].length === 1)
+                        return matching[1].call(this, val);
+                    else {}
+                }
+            }
+        };
+    };
+    
+    return {
+        on: on
+    }
+    /*var sum = $pattern_match.on(
+        [[],                  function () { return 0 }],
+        [rec_case('x', 'xs'), function () { return x + sum (xs) }]
+    );*/
+
+    /*var fact = fun(
+        [0, function ()  1],
+        [wc, function (n) n * fact(n - 1)]
+    );*/
+})();
